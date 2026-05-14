f45469e405af0fa7797e9f4fb670c82514fc7997
Filip Materowski <filip.materowski@gmail.com>
2026-05-14 17:24:52 +0200

    Dodanie klasy TParam

---
+++ AlgorytmGen/TParam.cpp
@@ -0,0 +1,39 @@
+#include "TParam.h"
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+TParam::TParam(double x_start, double x_end, double dx)
+{
+	set_range(x_start, x_end, dx);
+	name = "";
+	val_id = 0;
+}
+TParam::TParam(double x_start, double x_end, double dx, double val)
+{
+	set_range(x_start, x_end, dx);
+	name = "";
+	set_val(val);
+}
+
+void TParam::set_range(double x_start, double x_end, double dx)
+{
+	this->x_start = x_start;
+	this->x_end = x_end;
+	this->dx = dx;
+}
+
+int TParam::get_val_Id(double val) {
+	if (val < x_start) return 0;
+	else if (val > x_end) return (x_end - x_start) / dx;
+
+	else {
+		double x = x_start;
+		int _id = 0;
+		while (fabs(x + _id * dx - val) > dx / 2) _id++;
+		
+		return _id;
+
+	}
+}
